42a9a20674cf25a07776dc6dd6bba11b3d4b0e84
troy-lamerton <troy.lamerton@gmail.com>
2016-05-05 20:48:38 +1200

    Add enter key handler and focus input on page load

--- README.md
+++ README.md
@@ -4,20 +4,22 @@ Ollie and Troys Javascript calculator
 ## Required functionality
 ### User is able to:
 
-1. Add, subtract, multiply and divide two numbers.
+[X] 1. Add, subtract, multiply and divide two numbers.
 
-2. Clear the input field with a clear button.
+[X] 2. Clear the input field with a clear button.
 
-3. Chain mathematical operations together until they hit the equals button, and the calculator will output the correct value.
+[X] 3. Chain mathematical operations together until they hit the equals button, and the calculator will output the correct value.
 
 
 ## Extra ideas
-1. Brackets can be used in the calculation.
+[X] 1. Brackets can be used in the calculation.
 
-2. Be able to continue running calculations after the first one.
+[X] 2. Be able to continue running calculations after the first one.
 
-3. User-friendly stylish GUI
+[X] 3. User-friendly stylish GUI
 
-4. Calculations can be typed into the output box
+[X] 4. Calculations can be typed into the output box
 
-5. Add in history of equation
+[] 5. Add in history of equation
+
+[X] 6. Pressing enter calculates the formula. When the page loads, the textbox is in focus.
--- calculator.js
+++ calculator.js
@@ -1,34 +1,35 @@
-	$(document).ready(function() {
-		
-
+$(document).ready(function() {
+	$('#output-value').focus();
+	document.addEventListener('keyup', enterUpHandler, false);
+	function enterUpHandler (e) {
+		if (e.keyCode === 13) {
+			calculate ($('#output-value').val());
+		}
+	}
 	function calculate(equ){
 
 		$("#output-value").val(eval(equ));
 
 	}
 
-	// $("#equals").click(function() {
-	// 	calculate();
-	// })
+	$("button").click(function() {
 
-		$("button").click(function() {
+		var num = $('#output-value').val();
+		var button = $(this).attr("value");
 
-			var num = $('#output-value').val();
-			var button = $(this).attr("value");
-
-			if(button === "=") {
-				calculate(num);
-			} else if(button === "AC") {
-				$('#output-value').val("");
-			} else if(button === "CE") {
-				$('#output-value').val(function() {
-					return num.slice(0, num.length-1)
-				});
-			} else {
-
+		if(button === "=") {
+			calculate(num);
+		} else if(button === "AC") { //Clear all
+			$('#output-value').val("");
+		} else if(button === "CE") { //Backspace
+			$('#output-value').val(function() {
+				return num.slice(0, num.length-1)
+			});
+		} else { //Numbers and operators
 			$('#output-value').val( num + button);
 		}
-		});
+
+	});
 
 
-	})
+})
